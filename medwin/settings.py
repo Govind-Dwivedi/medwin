610d5b8f31d90799a0399a130d4376bcd9c20eca
Govind <govvedi@gmail.com>
2022-11-09 20:29:22 +0530

    database changed to mysql from sqlite3

--- medwin/settings.py
+++ medwin/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'medwin.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'medwin_db',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
